d5ec41220c2a2fe742e0b60f9cb2d88a2acfd122
Brian T. <bptiburcio@gmail.com>
2024-11-13 21:25:55 -0500

    Add transparency on macos

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,25 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 mod encryption;
 use encryption::{decrypt, encrypt, generate_master_password, constant_time_compare, generate_random_salt, generate_file_keys, derive_key};
+use tauri::Manager;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+    .setup(|app| {
+        if let Some(_window) = app.get_webview_window("main") {
+            #[cfg(target_os = "macos")]
+            unsafe {
+                window.with_webview(|webview| {
+                    webview.set_transparent(true);
+                }).unwrap();
+            }
+        }
+        Ok(())
+    })
+        .plugin(
+            tauri_plugin_global_shortcut::Builder::new().build()
+        )
         // .plugin(tauri_plugin_http::init())
         // .plugin(tauri_plugin_dialog::init())
         // .plugin(tauri_plugin_notification::init())
